36a328f5679897abb9b0f5efb1f790769abc793d
wiidede <wiixdede@gmail.com>
2023-08-01 10:32:04 +0800

    fix: type error

--- cloud.js
+++ cloud.js
@@ -8,26 +8,29 @@ const Comment = AV.Object.extend('Comment')
 async function sendMailByComment(comment) {
   const taskList = []
   let err = false
-  const status = comment.get('isNotified')
-  if (!status || status === 'false' || status === 'noticed') {
+  const isNotified = comment.get('isNotified')
+  const notifyStatus = comment.get('notifyStatus')
+  if (!isNotified || notifyStatus === 'noticed') {
     taskList.push(mail.notice(comment).catch((e) => {
       err = true
       console.error(`评论(${comment.get('objectId')})【${comment.get('comment')}】 通知站长失败 `, e)
     }).then(() => {
-      comment.set('isNotified', 'noticed')
+      comment.set('notifyStatus', 'noticed')
     }))
   }
-  if (!status || status === 'false' || status === 'sended') {
+  if (!isNotified || notifyStatus === 'sended') {
     taskList.push(mail.send(comment).catch((e) => {
       err = true
       console.error(`评论(${comment.get('objectId')})【${comment.get('comment')}】 发送被@者失败 `, e)
     }).then(() => {
-      comment.set('isNotified', 'sended')
+      comment.set('notifyStatus', 'sended')
     }))
   }
   await Promise.allSettled(taskList)
-  if (!err)
-    comment.set('isNotified', 'true')
+  if (!err) {
+    comment.set('isNotified', true)
+    comment.set('notifyStatus', 'finish')
+  }
 
   comment.save()
   if (err)
@@ -44,7 +47,6 @@ AV.Cloud.afterSave('Comment', async (request) => {
 AV.Cloud.define('resend_mails', async () => {
   const query = new AV.Query(Comment)
   query.greaterThanOrEqualTo('createdAt', new Date(new Date().getTime() - 24 * 60 * 60 * 1000))
-  query.notEqualTo('isNotified', 'true')
   query.notEqualTo('isNotified', true)
   // 如果你的评论量很大，可以适当调高数量限制，最高1000
   query.limit(200)
