49c9a6aa907223c0d810a4ee5ac828e29c24e3ba
wiidede <wiixdede@gmail.com>
2023-08-01 11:47:14 +0800

    chore: format

--- server-cluster.js
+++ server-cluster.js
@@ -1,17 +1,18 @@
-var cluster = require('cluster');
+const cluster = require('node:cluster')
+const process = require('node:process')
 
 // 进程数量建议设置为可用的 CPU 数量
-var workers = process.env.LEANCLOUD_AVAILABLE_CPUS || 1;
+const workers = process.env.LEANCLOUD_AVAILABLE_CPUS || 1
 
 if (cluster.isMaster) {
-  for (var i = 0; i < workers; i++) {
-    cluster.fork();
-  }
+  for (let i = 0; i < workers; i++)
+    cluster.fork()
 
-  cluster.on('exit', (worker, code, signal) => {
-    console.log('worker %s died, restarting...', worker.process.pid);
-    cluster.fork();
-  });
-} else {
-  require('./server.js');
+  cluster.on('exit', (worker) => {
+    console.log('worker %s died, restarting...', worker.process.pid)
+    cluster.fork()
+  })
+}
+else {
+  require('./server.js')
 }
