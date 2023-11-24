99aec49aba6d7194608cab431b4dafe9e7774f6a
wiidede <wiixdede@gmail.com>
2023-11-24 13:54:17 +0800

    feat: try to merge anti spam

--- routes/comments.js
+++ routes/comments.js
@@ -2,6 +2,8 @@
 const process = require('node:process')
 const router = require('express').Router()
 const AV = require('leanengine')
+const mail = require('../utilities/send-mail')
+const spam = require('../utilities/check-spam')
 
 const Comment = AV.Object.extend('Comment')
 
@@ -14,14 +16,12 @@ router.get('/', (req, res, next) => {
     query.find().then((results) => {
       res.render('comments', {
         title: `${process.env.SITE_NAME}上的评论`,
-        domain: process.env.SITE_URL,
         comment_list: results,
       })
     }, (err) => {
       if (err.code === 101) {
         res.render('comments', {
           title: `${process.env.SITE_NAME}上的评论`,
-          domain: process.env.SITE_URL,
           comment_list: [],
         })
       }
@@ -35,15 +35,58 @@ router.get('/', (req, res, next) => {
   }
 })
 
+router.get('/resend-email', (req, res, next) => {
+  if (req.currentUser) {
+    const query = new AV.Query(Comment)
+    query.get(req.query.id).then((object) => {
+      query.get(object.get('rid')).then((parent) => {
+        mail.send(object, parent)
+        res.redirect('/comments')
+      }).catch(next)
+    }).catch(next)
+  }
+  else {
+    res.redirect('/')
+  }
+})
+
 router.get('/delete', (req, res, next) => {
   if (req.currentUser) {
     const query = new AV.Query(Comment)
     query.get(req.query.id).then((object) => {
       object.destroy()
       res.redirect('/comments')
-    }, (err) => {
-      if (err)
-        console.error(err)
+    }).catch(next)
+  }
+  else {
+    res.redirect('/')
+  }
+})
+
+router.get('/not-spam', (req, res, next) => {
+  if (req.currentUser) {
+    const query = new AV.Query(Comment)
+    query.get(req.query.id).then((object) => {
+      object.set('isSpam', false)
+      object.set('ACL', { '*': { read: true } })
+      object.save()
+      spam.submitHam(object)
+      res.redirect('/comments')
+    }).catch(next)
+  }
+  else {
+    res.redirect('/')
+  }
+})
+router.get('/mark-spam', (req, res, next) => {
+  if (req.currentUser) {
+    const query = new AV.Query(Comment)
+    query.get(req.query.id).then((object) => {
+      object.set('isSpam', true)
+      object.set('ACL', { '*': { read: false } })
+      object.save()
+      spam.submitSpam(object)
+      res.redirect('/comments')
     }).catch(next)
   }
   else {
